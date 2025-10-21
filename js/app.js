194882e89cab73d91edeb671faf7719c189de399
Stewart-Chen <sob10938@gmail.com>
2025-10-21 17:31:10 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1464,7 +1464,7 @@ window.renderCourses    = window.renderCourses    || renderCourses;
       const nextState = {
         page: 1,
         teacher: null,
-        category: category ? [category] : null,
+        category: '園藝',
         plan_type: null,    // 不帶方案（避免互相干擾）
         keyword: null,
         q: ''
